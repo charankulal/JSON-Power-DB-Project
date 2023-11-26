43df1cfca96702aa1a75969c69cab313d8df1889
Charan <charankulal0241@gmail.com>
2023-11-26 21:32:10 +0530

    Created SaveEmployeeForm

--- public_html/index.js
+++ public_html/index.js
@@ -1,12 +1,4 @@
 const resetEmployeeForm=()=>{
-    // alert("Clicked")
-    // document.getElementById('empId').value = "";
-    // document.getElementById('empName').value = "";
-    // document.getElementById('empSalary').value = "";
-    // document.getElementById('hra').value = "";
-    // document.getElementById('da').value = "";
-    // document.getElementById('deduction').value = "";
-
     $('#empId').val("");
     $('#empName').val("");
     $('#empSalary').val("");
@@ -18,5 +10,74 @@ const resetEmployeeForm=()=>{
     $('#changeButton').prop("disabled",true);
     $('#resetButton').prop("disabled",true);
     $('#empId').focus();
+
+}
+
+const validateEmployeForm=()=>{
+    var empid=$('#empId').val().trim();
+    var empName=$('#empName').val().trim();
+    var empSalary=$('#empSalary').val().trim();
+    var hra=$('#hra').val().trim();
+    var da=$('#da').val().trim();
+    var deduction=$('#deduction').val().trim();
+    if(empid==""){
+        alert("Please enter employee id!");
+        $('#empId').focus();
+        return "";
+    }
+    if(empName==""){
+        alert("Please enter employee name!");
+        $('#empName').focus();
+        return "";
+    }
+    if(empSalary==""){
+        alert("Please enter employee salary!");
+        $('#empSalary').focus();
+        return "";
+    }
+    if(hra==""){
+        alert("Please enter HRA!");
+        $('#hra').focus();
+        return "";
+    }
+    if(da==""){
+        alert("Please enter DA!");
+        $('#da').focus();
+        return "";
+    }
+    if(deduction==""){
+        alert("Please enter Deduction!");
+        $('#deduction').focus();
+        return "";
+    }
+    var empData={
+        id:empid,
+        name:empName,
+        salary:empSalary,
+        hra:hra,
+        da:da,
+        deduction:deduction
+    }
+    console.log(JSON.stringify(empData))
+    return JSON.stringify(empData)
+
+}
+
+const saveEmployeeForm=()=>{
+    // alert("Clicked")
+    let ValidForm=validateEmployeForm();
+    if(ValidForm=="")
+    {
+        return ""
+    }
+    var putReqStr = createPUTRequest("90931496|-31949302813772162|90960247",
+ValidForm, "EMPLOYEE", "EMP-REL");
+alert(putReqStr);
+jQuery.ajaxSetup({async: false});
+var resultObj = executeCommandAtGivenBaseUrl(putReqStr,
+"http://api.login2explore.com:5577", "/api/iml");
+alert(JSON.stringify(resultObj));
+jQuery.ajaxSetup({async: true});
+resetEmployeeForm();
 
 }
